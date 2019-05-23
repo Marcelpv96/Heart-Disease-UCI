832e4c46b808408c2a188e9834230d3b044e2296
Marcel Porta Vallés <marcelpv96@gmail.com>
2019-05-23 12:04:38 +0200

    pre-processing

---
+++ pre_processing.R
@@ -0,0 +1,20 @@
+# MVA PROJECT STEP (1)
+#
+#
+# Pre-process of data. The student will perform a first summary of the
+# data, and, eventually, detect errors, outliers and missing values and
+# take the appropriate measures of correction. According to the
+# problem and data, it may be necessary to perform a selection of
+# variables (feature selection) and /or a derivation of new explanatory
+# variables (feature extraction) if the problem needs it.
+#
+
+
+heart_disease = read.csv("data/heart.csv")
+
+
+# Find missing variables
+
+if(which(is.na(heart_disease))){
+  heart_disease <- knnImputation(heart_disease, k = 1, scale = T)
+}
