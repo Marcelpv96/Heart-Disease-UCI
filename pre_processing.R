f6d1b16a3b5a979f11e2218396fa231078ac4c50
javiferran <javiferran@hotmail.com>
2019-05-31 02:11:36 +0200

    pre-processing advances

--- pre_processing.R
+++ pre_processing.R
@@ -9,14 +9,41 @@
 # variables (feature extraction) if the problem needs it.
 #
 
+library(chemometrics)
+library(DMwR)
+library(mice)
+library(missForest)
+library(ggplot2)
+
+
 #Shift + cmd + c
 setwd("/Users/JaviFerrando/Desktop/MVA-Project")
 
 heart_disease = read.csv("data/heart.csv")
 
+# Find missing variables
+which(is.na(heart_disease))
+
+#No missing values detected
+#No need to perform any imputation
+
 
 # Find missing variables
 
-if(which(is.na(heart_disease))){
-  heart_disease <- knnImputation(heart_disease, k = 1, scale = T)
-}
+# if(which(is.na(heart_disease))){
+#   heart_disease <- knnImputation(heart_disease, k = 1, scale = T)
+# }
+
+classVar <- lapply(heart_disease,class)   # class of each variable
+
+#Outlier detection
+############################
+Moutlier(heart_disease[,-14], quantile = 0.975, plot = TRUE, tol=1e-36) #Doesn't work
+
+
+#Local Outlier Factor
+outlier.scores <- lofactor(heart_disease[,-14], k=5)
+plot(density(outlier.scores),main='Distribution of individuals local outlier factor scores')
+
+
+
